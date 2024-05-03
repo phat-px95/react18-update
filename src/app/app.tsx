802c82da6af3d6eed7dc627d7500af0e985ee63e
XuanPhat <phat.pham@buckzy.net>
2024-05-02 22:50:20 -0400

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react18-update" />
+    </div>
+  );
+}
+
+export default App;
